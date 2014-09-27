ee25147de575c441d420c5f9e8bbc9f2b3f4cbf7
Fernando Flores <ferflores@gmail.com>
2014-09-27 13:14:42 -0500

    notifications test, splash screen and jq mobile added

--- js/index.js
+++ js/index.js
@@ -37,13 +37,52 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+       /* var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        console.log('Received Event: ' + id);*/
+        //navigator.splashscreen.show();
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(app.notificationReceived, app.notificationError,{"senderID":"475582331261","ecb":"app.onNotificationGCM"});
+
+    },
+
+    notificationReceived: function(result){
+        alert('Callback Success! Result = '+result)
+    },
+
+    notificationError: function(error){
+        alert('error: '+ error);
+    },
+
+    onNotificationGCM: function(e){
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
     }
+
 };
